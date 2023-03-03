92aa4f4066c38761fb447ba6ab1c8892622c8c70
Taha <staha.khiabani@gmail.com>
2023-03-03 17:47:01 +0200

    make the Customer and Subscription and Invoice model

--- Subscription_Task/finance/models.py
+++ Subscription_Task/finance/models.py
@@ -1,4 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    credit = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Subscription(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, primary_key=True)
+    cost = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Invoice(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    subscription_type = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return str(self.id)
